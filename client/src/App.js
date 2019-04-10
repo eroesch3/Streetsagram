18381495177b63e5eabf597ba4e9c910f4690e82
junipera <juniper.alcorn@gmail.com>
2019-04-10 19:52:26 -0400

    pseudocode, feed component, clean out app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Link, Route } from 'react-router-dom'
+/* import components here */
+
+/* global variables here */
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+/*set state here */
+    }
+/*bind functions here */
+  }
+
+/* write functions here */
+
   render() {
     return (
       <div className="App">
         <h1 className='Header'>Streetstagram home page test!</h1>
+
+          {/*create container div for routes
+          define 'switch'
+          */}
+
+          {/* route paths: use "buildings-app" assignment for model */}
       </div>
     );
   }
