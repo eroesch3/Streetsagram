7c2282199fe420fa30dd58b6d11ec21bbc4ad6d4
galbertoneil <goneilp@gmail.com>
2019-04-11 15:08:08 -0400

    adding urls to photos and updating the names

--- scripts/seedDb.js
+++ scripts/seedDb.js
@@ -35,31 +35,31 @@ const main = async() => {
     // Add seed here - photos
 
     const photo1 = await Photos.create({
-       images:'',
-       description:'La misma pelota',
+       images:'https://imgur.com/laB0X2v',
+       description:'Graffiti2',
        street:'lafayette st.',
        croosStreet:'broadway ',
        filter:''
     });
 
     const photo2 = await Photos.create({
-        images:'',
-        description:'Nice shoots nice shorts',
+        images:'https://imgur.com/rKnzY5O',
+        description:'Tun tun tunnel',
         street:'Bedford St.',
         croosStreet:'N7',
         filter:''
      });
 
      const photo3 = await Photos.create({
-        images:'',
-        description:'Shaking Shaking',
+        images:'https://imgur.com/adQW7nt',
+        description:'Mad City',
         street:'7th Ave.',
         croosStreet:'36th St.',
         filter:''
      });
 
      const photo4 = await Photos.create({
-        images:'',
+        images:'https://imgur.com/SIwSh1f',
         description:'Doin ma thang',
         street:'Northen Blvd.',
         croosStreet:'43th St.',
@@ -67,10 +67,10 @@ const main = async() => {
      });
 
      const photo5 = await Photos.create({
-        images:'Doin ya thang',
-        description:'Southern Blvd.',
-        street:'87th st.',
-        croosStreet:'',
+        images:'https://imgur.com/jW9MrzQ',
+        description:'Doing ya thang',
+        street:'Southern Blvd.',
+        croosStreet:'87th st.',
         filter:''
      });
 
@@ -83,5 +83,5 @@ const main = async() => {
     await photo5.setUser(user2);
    
 
-
+    Process.exit()
 }
