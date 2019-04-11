30d8dc80c3d2391ebb516f5c8c06d9fede91acc3
junipera <juniper.alcorn@gmail.com>
2019-04-11 14:10:09 -0400

    set up example image uploader on feedjs

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -1,1 +1,33 @@
 import ImageUploader from 'react-images-upload';
+import React, {Component} from 'react'
+
+class Feed extends Component {
+    constructor(props){
+        super(props) 
+        this.state={
+            pictures: []
+        }
+        this.onDrop=this.onDrop.bind(this)
+    }
+    
+    onDrop(picture) {
+        this.setState({pictures: this.state.pictures.concat(picture)})
+    }
+
+    render(){
+
+        return(
+            <div className='feed--divContainer'>
+                <ImageUploader 
+                    withIcon={true}
+                    buttonText='Choose images'
+                    onChange={this.onDrop}
+                    imgExtension={['.jpg', '.png']}
+                    maxFileSize={5242880}
+                />
+            </div>
+        )
+    }
+}
+
+export default Feed
