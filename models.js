a26b088bd31113053a065ab106ec355d3cf5d2f5
Teigan H <teigan.hockman@gmail.com>
2019-04-12 12:02:58 -0400

    Update Models.js

--- models.js
+++ models.js
@@ -9,122 +9,40 @@ const db = new Sequelize({
 
 // Define Users model
 const Users = db.define('users', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        field: 'id'
-    },
-    username: {
-        type: Sequelize.STRING(20),
-        allowNull: false,
-        notEmpty: true,
-        field: 'username',
-        set: function(val) {
-            this.setDataValue('username', val.toLowerCase())
-        }
-    },    
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        isEmail: true, 
-        notEmpty: true,
-        field: 'email',
-        set: function(val) {
-            this.setDataValue('email', val.toLowerCase())
-        }
-    },
+    id: Sequelize.INTEGER,
+    username: Sequelize.STRING(20),
+    email: Sequelize.STRING,
     password_digest: Sequelize.STRING,
     timestamps: false,
 })
 
 //Define Photos model
 const Photos = db.define('photos', {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      field: 'id'
-    },
-    user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        field: 'user id'
-    },    
-    image: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        notEmpty: true,
-        field: 'images',
-    },    
-    description: {
-        type: Sequelize.STRING,
-        field: 'description',
-    },
-    street: {
-        type: Sequelize.STRING,
-        field: 'first street'
-    },    
-    cross_street: {
-        type: Sequelize.STRING,
-        field: 'cross street'
-    },
-    filter: {
-        type: Sequelize.STRING,
-        field: filter,
-    },
+    id: Sequelize.INTEGER,
+    user_id: Sequelize.INTEGER,  
+    image: Sequelize.STRING,    
+    description: Sequelize.STRING,
+    street: Sequelize.STRING,   
+    cross_street: Sequelize.STRING,
+    filter: Sequelize.STRING,
     timestamps: false,
 })
 
 //Define Comments model
 const Comments = db.define('comments', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        notEmpty: true,
-        field: 'id'
-      },
-    photo_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        notEmpty: true,
-        field: 'photo id'
-    },
-    user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        notEmpty: true,
-        field: 'user id'
-    },
-    comment: {
-        type: Sequelize.STRING,
-        field: 'comments',
-    },
+    id: Sequelize.INTEGER,
+    photo_id: Sequelize.INTEGER,
+    user_id: Sequelize.INTEGER,
+    comment: Sequelize.STRING,
     timestamps: false,
 })
 
 //Define Profiles model
 const Profiles = db.define('profiles', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        notEmpty: true,
-        field: 'id'
-      },
-    user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        notEmpty: true,
-        field: 'user id'
-    },
-    profile_desc: {
-        type: Sequelize.STRING,
-        field: 'profile description'
-    },
-    contact: {
-        type: Sequelize.STRING,
-        field: 'contact info',
-        next_perform: Sequelize.STRING,
-    },
+    id: Sequelize.INTEGER,
+    user_id: Sequelize.INTEGER,
+    profile_desc: Sequelize.STRING,
+    contact: Sequelize.STRING,
     timestamps: false,
 })
 
