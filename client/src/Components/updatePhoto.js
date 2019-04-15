cac3fc23e01e3db668871f00c26f7fdf3fac8067
junipera <juniper.alcorn@gmail.com>
2019-04-15 14:08:29 -0400

    create updatePhoto component

---
+++ client/src/Components/updatePhoto.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class UpdatePhoto extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+        //bind functions
+    }
+    //functions
+    render(){
+        return(
+            <div className='updatePhoto--container'>
+            test
+            </div>
+        )
+    }
+}
+export default UpdatePhoto
