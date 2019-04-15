b93720523c65a121bf538410285b24afb5f2a455
Ed Roesch <edroesch@gmail.com>
2019-04-15 15:32:22 -0400

    sets up routing between main page and post page

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-
-
-
-
-
 export class Nav extends Component {
   render() {
     return (
-      
 
       <div className='nav--mainNav--containAllLinks'>
-        <h1 className='logo--navLogo-takeToHomePage'><Link to='/Feed'>Streetstagram</Link></h1>
+        <h1 className='logo--navLogo-takeToHomePage'><Link to='/'>Streetstagram</Link></h1>
             <ul className='navList--links--takeToDifferentComponents'>
                 <li className='list--link--takeToLogin'><Link to='/LogIn'>Login</Link></li>
-                <li className='list--link--takeToFeed'><Link to='/Feed'>Feed</Link></li>
+                <li className='list--link--takeToFeed'><Link to='/'>Feed</Link></li>
                 <li className='list--link--takeToPost'><Link to='/Post'>Post</Link></li>
             </ul>
-
-           
-        
       </div>
     )
   }
