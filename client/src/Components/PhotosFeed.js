fc2edd29a129add23cbd51fbf14015c875557b58
galbertoneil <goneilp@gmail.com>
2019-04-15 20:02:04 -0400

    style updatePhoto, got rid of the update form on photofeed.js by comment it out

--- client/src/Components/PhotosFeed.js
+++ client/src/Components/PhotosFeed.js
@@ -20,16 +20,16 @@ class PhotosFeed extends Component {
               <div className='photoStream--photoContain--photoDetails--description'>{photo.description}</div>
               <div className='photoStream--photoContain--photoDetails--street'>{photo.street} X {photo.cross_street}</div>
             </div>
-        )
+        
             <div className='photoStream--photoContain--photoUD'>
-              <div className='photoStream--photoContain--photoUD--update'>
+              {/* <div className='photoStream--photoContain--photoUD--update'>
                 <UpdatePhoto 
                   photoId = {photo.id}
                   url={photo.url}
                   description={photo.description}
                   street={photo.street}
                   cross_street={photo.cross_street}/>
-              </div>
+              </div> */}
             </div>
           </div>
         )   
