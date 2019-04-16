a10ec74a872a36a681392dd5305da1851cefe2a9
junipera <juniper.alcorn@gmail.com>
2019-04-16 08:56:45 -0400

    basic list comment component structure

---
+++ client/src/Components/ListComment.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class ListComment extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div className='listComment--contain'>
+                test
+            </div>
+        )
+    }
+}
+export default ListComment 
