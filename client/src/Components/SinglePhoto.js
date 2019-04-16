7678acca6a1683dc02a32ed270e7d833caff2682
junipera <juniper.alcorn@gmail.com>
2019-04-16 11:39:11 -0400

    set state with axios call

--- client/src/Components/SinglePhoto.js
+++ client/src/Components/SinglePhoto.js
@@ -7,7 +7,6 @@ class SinglePhoto extends Component{
     constructor(props){
         super(props)
         this.state={
-            file: null,
             url: '',  
             description: '',  
             street: '',
@@ -15,21 +14,16 @@ class SinglePhoto extends Component{
             photo:[],
         }
     this.getPhoto=this.getPhoto.bind(this)
-    this.displayPhoto=this.displayPhoto.bind(this)
     }
     
     getPhoto = async () => {
-        console.log(this.props)
         axios
           .get(`http://localhost:3001/photo/${this.props.match.params.id}`)
           .then(response=> {
-              this.setState({
-                  url: response.data.image,
-                  description: response.data.description,
-                  street: response.data.street,
-                  cross_street: response.data.cross_street
-            })
-              console.log('get single photo response', response.data)
+              const photo=this.state.photo
+              const singlePhoto=response.data
+              const setPhoto=photo.push(singlePhoto)
+              return setPhoto
           })
           .catch((error)=> {
             console.log("Error:", error)
@@ -38,35 +32,30 @@ class SinglePhoto extends Component{
 
     componentDidMount(){
         this.getPhoto()
+        // this.displayPhoto()
     }
 
-    displayPhoto(){
-        const {photo} = this.state.photo
-        return photo.map((photo)=>
-          <div className='singlePhoto--photoContain' key = {photo.id}>
-            <img className='photoStream--photoContain--photo' src={photo.image}/>
-            <div className='singlePhoto--photoContain--photoDetails'>
-              <div className='photoStream--photoContain--photoDetails--description'>{photo.description}</div>
-              <div className='photoStream--photoContain--photoDetails--street'>{photo.street} X {photo.cross_street}</div>
-            </div>
-            <div className='photoStream--photoContain--photoUD'>
-              <div className='photoStream--photoContain--photoUD--update'>
-                <UpdatePhoto 
-                  photoId = {photo.id}
-                  url={photo.url}
-                  description={photo.description}
-                  street={photo.street}
-                  cross_street={photo.cross_street}/>
-              </div>
-            </div>
-          </div>
-        )}
-
     render(){
-        console.log('photo state', this.state.photo)
+        console.log('render state in singlephoto', this.state.photo)
         return(
-            <div className='SinglePhoto'>
-                {/* {this.displayPhoto()} */}
+            
+            <div className='singlePhoto--photoContain' >
+                {/* <img className='photoStream--photoContain--photo' src={photo.image}/>
+                <div className='singlePhoto--photoContain--photoDetails'>
+                    <div className='photoStream--photoContain--photoDetails--description'>{photo.description}</div>
+                    <div className='photoStream--photoContain--photoDetails--street'>{photo.street} X {photo.cross_street}</div>
+                </div>
+                <div className='photoStream--photoContain--photoUD'>
+                    <div className='photoStream--photoContain--photoUD--update'>
+                        <UpdatePhoto 
+                        photoId = {photo.id}
+                        url={photo.url}
+                        description={photo.description}
+                        street={photo.street}
+                        cross_street={photo.cross_street}/>
+                    </div>
+                </div> */}
+                <UpdatePhoto />
             </div>
         )
     }
